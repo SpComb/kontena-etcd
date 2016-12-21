9b0549a0804ac1be36b4defc1160897d0aa182d6
Tero Marttila <tero.marttila@kontena.io>
2016-12-21 15:17:16 +0200

    specs: use webmock

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -52,6 +52,9 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
+  # Use WebMock
+  WebMock.disable_net_connect!
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
