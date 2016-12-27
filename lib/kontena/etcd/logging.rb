ffba8e9b93518fcfd151c67c37711cb5ece01b32
Tero Marttila <tero.marttila@kontena.io>
2016-12-27 19:16:28 +0200

    Kontena::Etcd::Logging: copy-pasta update

--- lib/kontena/etcd/logging.rb
+++ lib/kontena/etcd/logging.rb
@@ -1,16 +1,42 @@
 require 'logger'
 
+# per-class logger, using the class name as the progname
+#
+# Use LOG_LEVEL=debug to configure
 module Kontena::Etcd::Logging
-  def logger!
-    logger = Logger.new(STDERR)
-    logger.level = Logger::DEBUG
-    logger.progname = self.class.name
+  @log_level = ENV.fetch('LOG_LEVEL', Logger::INFO)
 
-    return logger
+  def self.log_level
+    @log_level
+  end
+  def log_level=(level)
+    @log_level = level
+  end
+
+  module ClassMethods
+    def log_level=(level)
+      @log_level = level
+    end
+
+    def logger(progname, level: nil)
+      logger = Logger.new(STDERR)
+      logger.level = level || @log_level || Kontena::Etcd::Logging.log_level
+      logger.progname = progname
+
+      return logger
+    end
+  end
+
+  def self.included(base)
+    base.extend ClassMethods
   end
 
   # @return [Logger]
-  def logger
-    @logger ||= self.logger!
+  def logger(progname = nil, **opts)
+    if progname
+      self.class.logger progname, **opts
+    else
+      @logger ||= self.class.logger self.class.name
+    end
   end
 end
